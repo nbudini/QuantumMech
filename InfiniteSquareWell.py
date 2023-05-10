00c6570811743aac18d93e5e1781703fff2f18f9
nbudini <67848517+nbudini@users.noreply.github.com>
2023-05-10 17:50:02 -0300

    Update InfiniteSquareWell.py

--- InfiniteSquareWell.py
+++ InfiniteSquareWell.py
@@ -1,47 +1,51 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  6 11:51:43 2023
-
-@author: nico
-"""
 %reset
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import pylab as p
 
+# constants
 m = 1 # 9.1e-31
 a = 1 # 1e-9
 A = np.sqrt(2/a)
 hbar = 1 # 6.626e-34
 
+# positions
 x = np.linspace(0, a, 500)
 
+# stationary states
 psi1 = A*np.sin(np.pi/a*x)
 psi2 = A*np.sin(2*np.pi/a*x)
+# add as many as desired, with psin = A*np.sin(n*np.pi/a*x)
 
+# energy eigenvalues of stationary states
 E1 = (1**2)*(np.pi**2)*(hbar**2)/(2*m*(a**2))
 E2 = (2**2)*(np.pi**2)*(hbar**2)/(2*m*(a**2))
+# add as many as desired, with En = (n**2)*(np.pi**2)*(hbar**2)/(2*m*(a**2))
 
+# coefficients of the expansion Psi(x,0) = sum_i c_i psi_i for the initial state Psi(x,0)
 c1 = 1/np.sqrt(2)
 c2 = np.sqrt(1 - c1**2)
 
-t = 0
-
-while t <= 1:
+# time evolution
+tsim = 10 # simulation time 
+dt = 0.01 # timestep
+while t <= tsim:
+    # Psi(x,t)
     psi = c1*psi1*np.exp(-1j*E1*t/hbar) + c2*psi2*np.exp(-1j*E2*t/hbar)
 
-    plt.plot(np.real(psi))
-    plt.plot(np.imag(psi))
-    plt.plot(np.abs(psi)**2)
+    plt.plot(np.real(psi))    # plot real part of wavefunction
+    plt.plot(np.imag(psi))    # plot imaginary part of wavefunction
+    plt.plot(np.abs(psi)**2)  # plot square modulus of wavefunction
     
-    plt.xlim(0,len(psi))
-    plt.ylim(-2,3.5)
+    plt.xlim(0,len(psi)) # set x limits
+    plt.ylim(-2,3.5)     # set y limites
 
+    # draw, (save if wanted), pause, clear
     plt.draw()
-    plt.savefig(str(round(t*100))+'.png')
+    # plt.savefig(str(round(t*100))+'.png')
     plt.pause(0.1)
     plt.clf()
     
-    t += 0.01
+    # advance time
+    t += dt
