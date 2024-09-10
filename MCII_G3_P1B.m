7bb8cd162b081e107f08e37eb4144bfa42e7606d
nbudini <67848517+nbudini@users.noreply.github.com>
2024-09-10 18:08:14 -0300

    Update MCII_G3_P1B.m

--- MCII_G3_P1B.m
+++ MCII_G3_P1B.m
@@ -7,6 +7,8 @@ Hpkn = sin((k-1)/2*pi)./(k-1)-sin((k+1)/2*pi)./(k+1); % elementos H'
 figure
 plot(k,Hpkn,'-*')
 xlim([0 100])
+xlabel('k')
+ylabel('<k⁰|Hp|1⁰>')
 
 % se calcula la suma de |H'k1|²/(1-k²) hasta cada valor de k
 for i = 1:length(Hpkn)
@@ -16,3 +18,5 @@ end
 % se muestra la evolución de la suma
 figure
 plot(k,suma)
+xlabel('k')
+ylabel('suma')
